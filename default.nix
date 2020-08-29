a8cfe4af078f9a4cd0c1a39ca7d1595d0c6e2b29
Jechol Lee <mr.jechol@gmail.com>
2020-08-29 10:47:35 +0900

    Copied from nixpkgs

--- default.nix
+++ default.nix
@@ -8,14 +8,26 @@
 
 { pkgs ? import <nixpkgs> {} }:
 
-{
+rec {
   # The `lib`, `modules`, and `overlay` names are special
   lib = import ./lib { inherit pkgs; }; # functions
   modules = import ./modules; # NixOS modules
   overlays = import ./overlays; # nixpkgs overlays
 
-  example-package = pkgs.callPackage ./pkgs/example-package { };
-  # some-qt5-package = pkgs.libsForQt5.callPackage ./pkgs/some-qt5-package { };
-  # ...
+  beam = pkgs.callPackage ./pkgs/top-level/beam-packages.nix { };
+
+  inherit (beam.interpreters)
+    erlang erlangR23 erlangR22 erlangR21 erlangR20 erlangR19 erlangR18
+    erlang_odbc erlang_javac erlang_odbc_javac erlang_nox erlang_basho_R16B02
+    elixir elixir_1_10 elixir_1_9 elixir_1_8 elixir_1_7 elixir_1_6;
+
+  inherit (beam.packages.erlang)
+    rebar rebar3
+    fetchHex beamPackages
+    relxExe;
+
+  inherit (beam.packages.erlangR19) cuter lfe_1_2;
+
+  inherit (beam.packages.erlangR21) lfe lfe_1_3;
 }
 
