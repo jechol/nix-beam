3239a9e2b8ffe06eaf3f604bffdd0a946b008d37
Jechol Lee <mr.jechol@gmail.com>
2020-09-01 12:11:53 +0900

    Elixir 1.8.0

--- pkgs/development/interpreters/elixir/all-versions.nix
+++ pkgs/development/interpreters/elixir/all-versions.nix
@@ -5,7 +5,7 @@ let
   beamLib = callPackage ../../beam-modules/lib.nix { };
 
   # majorVersions = [ ./1.10 ./1.9 ./1.8 ./1.7 ./1.6 ];
-  majorVersions = [ ./1.10 ./1.9 ];
+  majorVersions = [ ./1.10 ./1.9 ./1.8 ];
 
   deriveElixirs = releases:
     let
