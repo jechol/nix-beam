7ae2bc7ce3b2902e24a3cd3defee72da9a1cc440
Jechol Lee <mr.jechol@gmail.com>
2020-09-01 09:41:28 +0900

    beam.erlangs.r23.erlang_23_0_3 works!

--- pkgs/development/interpreters/erlang/R23/default.nix
+++ pkgs/development/interpreters/erlang/R23/default.nix
@@ -1,6 +1,7 @@
 { lib, wxGTK30, beamLib, util, mainOnly ? false }:
 
 let
+
   buildOpts = {
     wxGTK = wxGTK30;
     # Can be enabled since the bug has been fixed in https://github.com/erlang/otp/pull/2508
@@ -15,22 +16,26 @@ let
 
   makeVariants = release:
     let
-      basePkg = builtins.trace (beamLib.callErlang release buildOpts)
-        (beamLib.callErlang release buildOpts);
+      basePkg = beamLib.callErlang release buildOpts;
       featureStringToFlags = util.featureCombination featureOpts "_";
-      makePkg = featureString: featureFlags:
+      makePkg = featureString: featureFlag:
         let
           # pkgPath = util.makePkgPath "erlang" release.version featureString;
           # pkgName = util.makePkgName "erlang" release.version featureString;
           pkgPath = util.makePkgPath "erlang" basePkg.version featureString;
           pkgName = util.makePkgName "erlang" basePkg.version featureString;
 
-          featurePkg = basePkg.override featureFlags;
+          featurePkg = basePkg.override featureFlag;
           namedPkg = featurePkg.overrideAttrs (o: { name = pkgName; });
-        in namedPkg;
-    in lib.attrsets.mapAttrs makePkg featureStringToFlags;
+        in {
+          name = pkgPath;
+          value = namedPkg;
+        };
+
+    in lib.attrsets.mapAttrs' makePkg featureStringToFlags;
 
   releases = [ ./R23.nix ];
   nestedVariants = map makeVariants releases;
-  flatVariants = builtins.foldl' (acc: attrs: acc // attrs) nestedVariants;
+  flatVariants = builtins.foldl' (acc: attrs: acc // attrs) { } nestedVariants;
+
 in flatVariants
--- pkgs/development/interpreters/erlang/all-versions.nix
+++ pkgs/development/interpreters/erlang/all-versions.nix
@@ -8,41 +8,8 @@ rec {
 
   # releaseAttrs = builtins.foldl' (acc: release: acc // release) { } releases;
 
+  r23 = callPackage ./R23 { inherit beamLib util mainOnly; };
+
   # in releaseAttrs
 
-  buildOpts = {
-    wxGTK = wxGTK30;
-    # Can be enabled since the bug has been fixed in https://github.com/erlang/otp/pull/2508
-    parallelBuild = true;
-  };
-
-  featureOpts = {
-    odbc = { odbcSupport = true; };
-    javac = { javacSupport = true; };
-    nox = { wxSupport = false; };
-  };
-
-  makeVariants = release:
-    let
-      basePkg = beamLib.callErlang release buildOpts;
-      featureStringToFlags = util.featureCombination featureOpts "_";
-      makePkg = featureString: featureFlag:
-        let
-          # pkgPath = util.makePkgPath "erlang" release.version featureString;
-          # pkgName = util.makePkgName "erlang" release.version featureString;
-          pkgPath = util.makePkgPath "erlang" basePkg.version featureString;
-          pkgName = util.makePkgName "erlang" basePkg.version featureString;
-
-          featurePkg = basePkg.override featureFlag;
-          namedPkg = featurePkg.overrideAttrs (o: { name = pkgName; });
-        in {
-          name = pkgPath;
-          value = namedPkg;
-        };
-
-    in lib.attrsets.mapAttrs' makePkg featureStringToFlags;
-
-  releases = [ ./R23.nix ];
-  nestedVariants = map makeVariants releases;
-  flatVariants = builtins.foldl' (acc: attrs: acc // attrs) { } nestedVariants;
 }
