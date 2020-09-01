b0f89bf41700debd03ada6c4c52324ba39b1eecd
Jechol Lee <mr.jechol@gmail.com>
2020-09-01 09:53:57 +0900

    Refactor R23/default.nix

--- pkgs/development/interpreters/erlang/R23/default.nix
+++ pkgs/development/interpreters/erlang/R23/default.nix
@@ -1,7 +1,6 @@
-{ lib, wxGTK30, beamLib, util, mainOnly ? false }:
+{ lib, wxGTK30, beamLib, util, deriveErlangFeatureVariants, mainOnly ? false }:
 
 let
-
   buildOpts = {
     wxGTK = wxGTK30;
     # Can be enabled since the bug has been fixed in https://github.com/erlang/otp/pull/2508
@@ -16,26 +15,9 @@ let
     nox = { wxSupport = false; };
   };
 
-  featureStringToFlags = util.featureCombination featureOpts "_";
-
-  overrideFeature = basePkg: featureString: featureFlag:
-    let
-      pkgPath = util.makePkgPath "erlang" basePkg.version featureString;
-      pkgName = util.makePkgName "erlang" basePkg.version featureString;
-
-      featurePkg = basePkg.override featureFlag;
-      namedPkg = featurePkg.overrideAttrs (o: { name = pkgName; });
-    in {
-      name = pkgPath;
-      value = namedPkg;
-    };
-
-  makeVariants = release:
-    let basePkg = beamLib.callErlang release buildOpts;
-    in lib.attrsets.mapAttrs' (overrideFeature basePkg) featureStringToFlags;
-
   releases = [ ./R23.nix ];
-  nestedVariants = map makeVariants releases;
-  flatVariants = builtins.foldl' (acc: attrs: acc // attrs) { } nestedVariants;
 
-in flatVariants
+  variantsPerReleases =
+    map (r: deriveErlangFeatureVariants r buildOpts featureOpts) releases;
+
+in builtins.foldl' (acc: attrs: acc // attrs) { } variantsPerReleases
--- pkgs/development/interpreters/erlang/all-versions.nix
+++ pkgs/development/interpreters/erlang/all-versions.nix
@@ -1,14 +1,35 @@
 { pkgs, fetchpatch, callPackage, wxGTK30, openssl_1_0_2, lib, util
 , mainOnly ? false }:
 
-rec {
-  # let
+let
   beamLib = callPackage ../../beam-modules/lib.nix { };
+
+  overrideFeature = basePkg: featureString: featureFlag:
+    let
+      pkgPath = util.makePkgPath "erlang" basePkg.version featureString;
+      pkgName = util.makePkgName "erlang" basePkg.version featureString;
+
+      featurePkg = basePkg.override featureFlag;
+      namedPkg = featurePkg.overrideAttrs (o: { name = pkgName; });
+    in {
+      name = pkgPath;
+      value = namedPkg;
+    };
+
+  deriveErlangFeatureVariants = release: buildOpts: featureOpts:
+    let
+      basePkg = beamLib.callErlang release buildOpts;
+      featureStringToFlags = util.featureCombination featureOpts "_";
+    in lib.attrsets.mapAttrs' (overrideFeature basePkg) featureStringToFlags;
+in rec {
+  # let
   # releases = [ (callPackage ./R23/default.nix { inherit beamLib util; }) ];
 
   # releaseAttrs = builtins.foldl' (acc: release: acc // release) { } releases;
 
-  r23 = callPackage ./R23 { inherit beamLib util mainOnly; };
+  r23 = callPackage ./R23 {
+    inherit beamLib util mainOnly deriveErlangFeatureVariants;
+  };
 
   # in releaseAttrs
 
