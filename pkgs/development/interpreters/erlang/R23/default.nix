a62b503a63752b6cce4cedd6e887a8ac2e30c202
Jechol Lee <mr.jechol@gmail.com>
2020-09-01 09:46:05 +0900

    Refactor R23/default.nix

--- pkgs/development/interpreters/erlang/R23/default.nix
+++ pkgs/development/interpreters/erlang/R23/default.nix
@@ -14,25 +14,23 @@ let
     nox = { wxSupport = false; };
   };
 
-  makeVariants = release:
+  featureStringToFlags = util.featureCombination featureOpts "_";
+
+  overrideFeature = basePkg: featureString: featureFlag:
     let
-      basePkg = beamLib.callErlang release buildOpts;
-      featureStringToFlags = util.featureCombination featureOpts "_";
-      makePkg = featureString: featureFlag:
-        let
-          # pkgPath = util.makePkgPath "erlang" release.version featureString;
-          # pkgName = util.makePkgName "erlang" release.version featureString;
-          pkgPath = util.makePkgPath "erlang" basePkg.version featureString;
-          pkgName = util.makePkgName "erlang" basePkg.version featureString;
+      pkgPath = util.makePkgPath "erlang" basePkg.version featureString;
+      pkgName = util.makePkgName "erlang" basePkg.version featureString;
 
-          featurePkg = basePkg.override featureFlag;
-          namedPkg = featurePkg.overrideAttrs (o: { name = pkgName; });
-        in {
-          name = pkgPath;
-          value = namedPkg;
-        };
+      featurePkg = basePkg.override featureFlag;
+      namedPkg = featurePkg.overrideAttrs (o: { name = pkgName; });
+    in {
+      name = pkgPath;
+      value = namedPkg;
+    };
 
-    in lib.attrsets.mapAttrs' makePkg featureStringToFlags;
+  makeVariants = release:
+    let basePkg = beamLib.callErlang release buildOpts;
+    in lib.attrsets.mapAttrs' (overrideFeature basePkg) featureStringToFlags;
 
   releases = [ ./R23.nix ];
   nestedVariants = map makeVariants releases;
