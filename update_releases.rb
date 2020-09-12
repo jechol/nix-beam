344d6a8302d8d9866575d2c053acf89278d4904f
Jechol Lee <mr.jechol@gmail.com>
2020-09-12 17:17:27 +0900

    bundle exec ruby ./update_releases.rb works!

---
+++ update_releases.rb
@@ -0,0 +1,72 @@
+# frozen_string_literal: true
+
+require 'octokit'
+
+default_template =
+
+  def get_tarball_url(r)
+    r[:assets].map { |a| a[:browser_download_url] }.filter { |url| url.end_with?('.tar.gz') }.first
+  end
+
+def get_version(r)
+  r[:tag_name].match(/([\d\.]+)/)[1]
+end
+
+def dir(version)
+  major_ver = version.match(/(\d+)/)[1]
+  "pkgs/development/interpreters/erlang/R#{major_ver}"
+end
+
+def nix_path(version)
+  "#{dir(version)}/R#{version}.nix"
+end
+
+def new_version?(version)
+  !File.exist?(nix_path(version))
+end
+
+def nix_prefetch_sha256(url)
+  output = `nix-prefetch-url --unpack #{url}`
+  output.strip
+end
+
+def template_path(version)
+  "#{dir(version)}/template.nix"
+end
+
+def get_template(version)
+  if File.exist?(template_path(version))
+    File.read(template_path(version))
+  else
+    <<~EOF
+      { mkDerivation }:
+
+      mkDerivation {
+        version = "<<VERSION>>";
+        sha256 = "<<SHA256>>";
+      }
+    EOF
+  end
+end
+
+def write_nix(version, sha256)
+  template = get_template(version)
+  content = template.gsub('<<VERSION>>', version).gsub('<<SHA256>>', sha256)
+  path = nix_path(version)
+  FileUtils.mkdir_p(File.dirname(path))
+  File.write(path, content)
+end
+
+client = Octokit::Client.new
+otp = client.repo 'erlang/otp'
+releases = otp.rels[:releases].get.data
+
+new_releases = releases.filter { |r| new_version?(get_version(r)) }.take(1)
+
+new_releases.each do |r|
+  version = get_version(r)
+  url = get_tarball_url(r)
+  sha256 = nix_prefetch_sha256(url)
+
+  write_nix(version, sha256)
+end
