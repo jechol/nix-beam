716b3e3d5941eac8e46eafd840ba4d23ab5f8ce0
Jechol Lee <mr.jechol@gmail.com>
2020-10-15 10:40:28 +0900

    Re-order deps

--- pkgs/development/tools/build-managers/rebar3/default.nix
+++ pkgs/development/tools/build-managers/rebar3/default.nix
@@ -1,10 +1,28 @@
-{ stdenv, fetchFromGitHub,
-  fetchHex, erlang,
-  tree }:
+{ stdenv, fetchFromGitHub, fetchHex, erlang, tree }:
 
 let
-  version = "3.12.0";
+  version = "3.14.1";
 
+  bbmustache = fetchHex {
+    pkg = "bbmustache";
+    version = "1.6.0";
+    sha256 = "53e02d296512a57be03a98c91541b34d2ca64930268030b2d12364a0332015df";
+  };
+  certifi = fetchHex {
+    pkg = "certifi";
+    version = "2.5.2";
+    sha256 = "3b3b5f36493004ac3455966991eaf6e768ce9884693d9968055aeeeb1e575040";
+  };
+  cf = fetchHex {
+    pkg = "cf";
+    version = "0.2.2";
+    sha256 = "08cvy7skn5d2k4manlx5k3anqgjdvajjhc5jwxbaszxw34q3na28";
+  };
+  cth_readable = fetchHex {
+    pkg = "cth_readable";
+    version = "1.4.3";
+    sha256 = "0wr0hba6ka74s3628jrrd7ynjdh7syxigkh7ildg8fgi20ab88fd";
+  };
   erlware_commons = fetchHex {
     pkg = "erlware_commons";
     version = "1.3.1";
@@ -12,13 +30,8 @@ let
   };
   ssl_verify_fun = fetchHex {
     pkg = "ssl_verify_fun";
-    version = "1.1.3";
+    version = "1.1.6";
     sha256 = "2e120e6505d6e9ededb2836611dfe2f7028432dc280957998e154307b5ea92fe";
-  };
-  certifi = fetchHex {
-    pkg = "certifi";
-    version = "2.3.1";
-    sha256 = "e12d667d042c11d130594bae2b0097e63836fe8b1e6d6b2cc48f8bb7a2cf7d68";
   };
   providers = fetchHex {
     pkg = "providers";
@@ -30,25 +43,10 @@ let
     version = "1.0.1";
     sha256 = "53e1ab83b9ceb65c9672d3e7a35b8092e9bdc9b3ee80721471a161c10c59959c";
   };
-  bbmustache = fetchHex {
-    pkg = "bbmustache";
-    version = "1.6.0";
-    sha256 = "53e02d296512a57be03a98c91541b34d2ca64930268030b2d12364a0332015df";
-  };
   relx = fetchHex {
     pkg = "relx";
     version = "3.28.0";
     sha256 = "8afb871c0a2a27f0063d973903fc64d2207bc705ecc3607462920683d24ac7b5";
-  };
-  cf = fetchHex {
-    pkg = "cf";
-    version = "0.2.2";
-    sha256 = "08cvy7skn5d2k4manlx5k3anqgjdvajjhc5jwxbaszxw34q3na28";
-  };
-  cth_readable = fetchHex {
-    pkg = "cth_readable";
-    version = "1.4.3";
-    sha256 = "0wr0hba6ka74s3628jrrd7ynjdh7syxigkh7ildg8fgi20ab88fd";
   };
   eunit_formatters = fetchHex {
     pkg = "eunit_formatters";
@@ -66,8 +64,7 @@ let
     sha256 = "0q5r871bzx1a8fa06yyxdi3xkkp7v5yqazzah03d6yl3vsmn7vqp";
   };
 
-in
-stdenv.mkDerivation rec {
+in stdenv.mkDerivation rec {
   pname = "rebar3";
   inherit version erlang;
 
@@ -115,7 +112,8 @@ stdenv.mkDerivation rec {
 
   meta = {
     homepage = "https://github.com/rebar/rebar3";
-    description = "Erlang build tool that makes it easy to compile and test Erlang applications, port drivers and releases";
+    description =
+      "Erlang build tool that makes it easy to compile and test Erlang applications, port drivers and releases";
 
     longDescription = ''
       rebar is a self-contained Erlang script, so it's easy to distribute or
@@ -124,7 +122,7 @@ stdenv.mkDerivation rec {
       of build configuration work. rebar also provides dependency management,
       enabling application writers to easily re-use common libraries from a
       variety of locations (hex.pm, git, hg, and so on).
-      '';
+    '';
 
     platforms = stdenv.lib.platforms.unix;
     maintainers = with stdenv.lib.maintainers; [ gleber tazjin ];
