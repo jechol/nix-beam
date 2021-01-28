5141babb8e13ca28ec9d9479da9086ecbc476053
Jechol Lee <mr.jechol@gmail.com>
2021-01-28 13:52:00 +0900

    elixir 1.11.*

--- pkgs/development/interpreters/elixir/1.11/default.nix
+++ pkgs/development/interpreters/elixir/1.11/default.nix
@@ -2,5 +2,5 @@
 
 let
   # releases = util.findByPrefix ./. (baseNameOf ./.);
-  releases = [ ./1.11.0.nix ];
+  releases = [ ./1.11.0.nix ./1.11.1.nix ./1.11.2.nix ./1.11.3.nix ];
 in deriveElixirs releases "21" "23"
