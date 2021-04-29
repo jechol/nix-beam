d424c86437c46723dab9d3bc0c137626d9c71c53
Jechol Lee <mr.jechol@gmail.com>
2021-04-29 12:07:02 +0900

    Add Elixir v1.11.4

--- pkgs/development/interpreters/elixir/1.11/default.nix
+++ pkgs/development/interpreters/elixir/1.11/default.nix
@@ -1,6 +1,4 @@
 { util, deriveElixirs }:
 
-let
-  # releases = util.findByPrefix ./. (baseNameOf ./.);
-  releases = [ ./1.11.0.nix ./1.11.1.nix ./1.11.2.nix ./1.11.3.nix ];
+let releases = util.findByPrefix ./. (baseNameOf ./.);
 in deriveElixirs releases "21" "23"
