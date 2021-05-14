568365905d1a1afda7a5eb3aa640482c63a41898
Jechol Lee <mr.jechol@gmail.com>
2021-05-15 00:10:08 +0900

    move wxSupport

--- pkgs/development/interpreters/erlang/R24/default.nix
+++ pkgs/development/interpreters/erlang/R24/default.nix
@@ -7,13 +7,13 @@ let
     wxGTK = wxGTK30;
     # Can be enabled since the bug has been fixed in https://github.com/erlang/otp/pull/2508
     parallelBuild = true;
+    wxSupport = true;
   };
 
   variantOpts = {
     "" = {
       odbcSupport = true;
       javacSupport = true;
-      wxSupport = true;
     };
   };
 
